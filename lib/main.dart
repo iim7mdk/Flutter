891931fde015b7a84830950694b28e7bd3bd1e93
Mohammed <m7md.k2003@gmail.com>
2025-11-23 22:01:38 +0300

    Buttom Navagation bar Branch Added

--- lib/main.dart
+++ lib/main.dart
@@ -10,184 +10,63 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'YelpCamp',
       debugShowCheckedModeBanner: false,
-      home: const MyHomePage(),
+      home: const HomeScreen(),
     );
   }
 }
 
-class MyHomePage extends StatelessWidget {
-  const MyHomePage({super.key});
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  int currentIndex = 0;
+
+  final List<Widget> pages = const [
+    Center(child: Text("الصفحة الرئيسية", style: TextStyle(fontSize: 24))),
+    Center(child: Text("صفحة البحث", style: TextStyle(fontSize: 24))),
+    Center(child: Text("صفحة الإعدادات", style: TextStyle(fontSize: 24))),
+    Center(child: Text("صفحة الحساب", style: TextStyle(fontSize: 24))),
+  ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      body: pages[currentIndex],
 
-      appBar: AppBar(
-        centerTitle: true,
-        title: const Text(
-          'CV',
-          style: TextStyle(color: Colors.white),
-        ),
-        backgroundColor: Colors.grey[900],
-      ),
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: currentIndex,
+        onTap: (index) {
+          setState(() {
+            currentIndex = index;
+          });
+        },
 
-      body: Container(
-        margin: const EdgeInsets.all(16),
-        padding: const EdgeInsets.all(16),
-        decoration: BoxDecoration(
-          color: Colors.white, // لون الخلفية داخل الـ Container
-          borderRadius: BorderRadius.circular(20), // الزوايا الدائرية
-          boxShadow: [
-            BoxShadow(
-              color: Colors.black12,
-              blurRadius: 10,
-              offset: Offset(0, 5),
-            ),
-          ],
-        ),
-        child: ListView(
-          children: const [
-            CircleAvatar(
-              radius: 80, // خفّضت الحجم ليكون مناسبًا داخل الكرت
-              backgroundImage: AssetImage('assets/Profile.jpg'),
-            ),
-            SizedBox(height: 16),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
-              children: [
-                Text(
-                  'Mohammed Ali Alkaff',
-                  style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-                ),
-              ],
-            ),
-            SizedBox(height: 8),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
-              children: [
-                Text(
-                  'Applications Developer',
-                  style: TextStyle(color: Colors.grey),
-                ),
-              ],
-            ),
-            Divider(
-              color: Colors.grey,
-              thickness: 1,
-              indent: 20,
-              endIndent: 20,
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Text('Email'),
-                Text(
-                  'M7md.k@email.com',
-                  style: TextStyle(color: Colors.grey),
-                ),
-              ],
-            ),
-            Divider(
-              color: Colors.grey,
-              thickness: 1,
-              indent: 20,
-              endIndent: 20,
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Text('Number'),
-                Text(
-                  '739404244',
-                  style: TextStyle(color: Colors.grey),
-                ),
-              ],
-            ),
-            Divider(
-              color: Colors.grey,
-              thickness: 1,
-              indent: 20,
-              endIndent: 20,
-            ),
-            SizedBox(height: 16),
-            Padding(
-              padding: EdgeInsets.symmetric(horizontal: 8.0),
-              child: Text(
-                'Skills',
-                style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-              ),
-            ),
-            SizedBox(height: 8),
-            Padding(
-              padding: EdgeInsets.symmetric(horizontal: 8.0),
-              child: Wrap(
-                spacing: 8,
-                runSpacing: 6,
-                children: [
-                  Chip(label: Text('C++')),
-                  Chip(label: Text('Java')),
-                  Chip(label: Text('C#')),
-                  Chip(label: Text('HTML')),
-                  Chip(label: Text('CSS')),
-                  Chip(label: Text('JavaScript')),
-                  Chip(label: Text('React')),
-                  Chip(label: Text('Flutter')),
-                  Chip(label: Text('Dart')),
-                  Chip(label: Text('REST API')),
-                  Chip(label: Text('SQL')),
-                  Chip(label: Text('Firebase')),
-                  Chip(label: Text('MongoDB')),
-                  Chip(label: Text('Node.js')),
-                  Chip(label: Text('Git')),
-                  Chip(label: Text('Github')),
-                  Chip(label: Text('UI/UX')),
-                  Chip(label: Text('Unit Testing')),
-                ],
-              ),
-            ),
-            SizedBox(height: 16),
-            Divider(
-              color: Colors.grey,
-              thickness: 1,
-              indent: 20,
-              endIndent: 20,
-            ),
-          ],
-        ),
-      )
+        type: BottomNavigationBarType.fixed,
+        selectedItemColor: Colors.blue,
+        unselectedItemColor: Colors.grey,
 
-
-    );
-  }
-}
-
-class CampCard extends StatelessWidget {
-  final String imageUrl;
-  final String title;
-  final String description;
-
-  const CampCard({
-    super.key,
-    required this.imageUrl,
-    required this.title,
-    required this.description,
-  });
-
-  @override
-  Widget build(BuildContext context) {
-    return Card(
-      elevation: 5,
-      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(15)),
-      clipBehavior: Clip.antiAlias,
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          // صورة المخيم
-
-
-
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: "الرئيسية",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.search),
+            label: "البحث",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.settings),
+            label: "الإعدادات",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person),
+            label: "الحساب",
+          ),
         ],
       ),
     );
